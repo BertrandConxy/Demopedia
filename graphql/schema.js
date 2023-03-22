4a75f8db32011f4b8e0896bd03cfb90848902f3f
Bertrand <mutanganabertrand@gmail.com>
2023-03-22 08:09:12 +0200

    Add Appolo server integration with Express

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,14 +1,43 @@
 import { gql } from 'graphql-tag';
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-
   type Query {
-    books: [Book]
+    countries: CountryList!
+    country(id: ID!): CountryOutput!
+  }
+  type Mutation {
+    addCountry(country: CountryInput): CountryOutput!
+    updateCountry(country: CountryUpdate): CountryOutput!
+    deleteCountry(id: ID!): CountryOutput!
+  }
+  type CountryList {
+    data: [Country]!
+    count: Int!
+    message: String
+  }
+  type Country {
+    id: ID!
+    area: Int!
+    country: String!
+    totalPopulation: Int!
+    year: String!
+  }
+  type CountryOutput {
+    country: Country
+    message: String
+  }
+  input CountryInput {
+    area: Int!
+    country: String!
+    totalPopulation: Int!
+    year: String!
+  }
+  input CountryUpdate {
+    id: ID!
+    area: Int!
+    country: String!
+    totalPopulation: Int!
+    year: String!
   }
 `;
-
-export default typeDefs
+export default typeDefs;
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 import "dotenv/config";
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 import typeDefs from "./graphql/schema.js";
 import resolvers from "./graphql/resolvers.js";
-
 import dbConnect from "./dbConfig/dbConnect.js";
-
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
-
+const app = express();
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     csrfPrevention: true,
     cache: "bounded",
   });
 
-const { url } = await startStandaloneServer(server, {
-listen: { port: 4000 },
-});
 
-dbConnect()
+await server.start();
+app.use(
+  '/api/countries',
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server)
+);
 
-console.log(`🚀  Server ready at: ${url}`);
+dbConnect() // connect to MongoDb Atlas database
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/api/countries`);
